151a7419cfa9ab20bddefa102322e49e8ac393a2
Yusuke Tanimoto <mariotan7@gmail.com>
2014-06-20 01:29:52 +0900

    インデントを調整

--- game.lisp
+++ game.lisp
@@ -17,10 +17,10 @@
                         (attic  (living-room downstairs ladder))))
 
 (defun describe-path (edge)
- `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
 
 (defun describe-paths (location edges)
- (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 
 (defparameter *objects* '(whiskey bucket frog chain))
 (defparameter *object-locations* '((whiskey living-room)
@@ -29,44 +29,44 @@
                                    (frog    garden     )))
 
 (defun objects-at (loc objs obj-locs)
-(labels ((at-loc-p (obj)
-           (eq (cadr (assoc obj obj-locs)) loc)))
-  (remove-if-not #'at-loc-p objs)))
+  (labels ((at-loc-p (obj)
+                     (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
 
 (defun describe-objects (loc objs obj-loc)
- (labels ((describe-obj (obj)
-           `(you see a ,obj on the floor.)))
-  (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+  (labels ((describe-obj (obj)
+                         `(you see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
 
 (defparameter *location* 'living-room)
 
 (defun look ()
- (append (describe-location *location* *nodes*)
-         (describe-paths    *location* *edges*)
-         (describe-objects  *location* *objects* *object-locations*)))
+  (append (describe-location *location* *nodes*)
+          (describe-paths    *location* *edges*)
+          (describe-objects  *location* *objects* *object-locations*)))
 
 (defun walk (direction)
- (let ((next (find direction
-              (cdr (assoc *location* *edges*))
-              :key #'cadr)))
-  (if next
-        (progn (setf *location* (car next))
-               (look))
-               '(you cannot go that way))))
+  (let ((next (find direction
+                    (cdr (assoc *location* *edges*))
+                    :key #'cadr)))
+    (if next
+      (progn (setf *location* (car next))
+             (look))
+      '(you cannot go that way))))
 
 (defun pickup (object)
- (cond ((member object
-         (objects-at *location* *objects* *object-locations*))
-        (push (list object 'body) *object-locations*)
-        `(you are now carrying the ,object ))
-      (t '(you cannnot get that.))))
+  (cond ((member object
+                 (objects-at *location* *objects* *object-locations*))
+         (push (list object 'body) *object-locations*)
+         `(you are now carrying the ,object ))
+        (t '(you cannnot get that.))))
 
 
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
 
 ;;(defun game-repl()
-  ;;(loop (print (eval (read)))))
+;;(loop (print (eval (read)))))
 
 (defun game-repl()
   (let ((cmd (game-read)))
