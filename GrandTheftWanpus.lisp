c4280c08a4ca6d14cacec73e838546405582f9da
Yusuke Tanimoto <mariotan7@gmail.com>
2014-06-21 04:01:49 +0900

    8.3まで終了

---
+++ GrandTheftWanpus.lisp
@@ -0,0 +1,210 @@
+(defun l()
+  (load "GrandTheftWanpus"))
+
+(load "graph-util")
+
+(defparameter *congestion-city-nodes* nil)
+(defparameter *congestion-city-edges* nil)
+(defparameter *visited-nodes*         nil)
+(defparameter *node-num* 30)
+(defparameter *edge-num* 45)
+(defparameter *worm-num* 3 )
+(defparameter *cop-odds* 15)
+
+;; ランダムなエッジの作成
+(defun random-node ()
+  (1+ (random *node-num*)))
+
+(defun edge-pair (a b)
+  (unless (eql a b)
+    (list (cons a b) (cons b a))))
+
+(defun make-edge-list ()
+  (apply #'append (loop repeat *edge-num*
+                        collect (edge-pair (random-node) (random-node)))))
+
+;;----------------------------
+;; 孤島を作らない
+;;----------------------------
+
+;; 与えられたノードを基点とする全てのエッジを返す関数
+(defun direct-edges (node edge-list)
+  (remove-if-not (lambda (x)
+                   (eql (car x) node))
+                 edge-list))
+
+;; 孤島を見つける
+;; 起点から到達可能な全てのノードのリストを作る
+(defun get-connected (node edge-list)
+  (let ((visited nil))
+    (labels ((traverse (node)
+                       (unless (member node visited)
+                         (push node visited)
+                         (mapc (lambda (edge)
+                                 (traverse (cdr edge)))
+                               (direct-edges node edge-list)))))
+      (traverse node))
+    visited))
+
+;; グラフから孤島を探す関数
+(defun find-islands (nodes edge-list)
+  (let ((islands nil))
+    (labels ((find-island (nodes)
+                          (let* ((connected   (get-connected (car nodes) edge-list))
+                                 (unconnected (set-difference nodes connected     )))
+                            (push connected islands)
+                            (when unconnected
+                              (find-island unconnected)))))
+      (find-island nodes))
+    islands))
+
+;; 島をつなぐ関数
+(defun connect-with-bridges (islands)
+  (when (cdr islands)
+    (append (edge-pair (caar islands) (caadr islands))
+            (connect-with-bridges (cdr islands)))))
+
+;; 孤島解消のための関数をまとめる
+(defun connect-all-islands (nodes edge-list)
+  (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
+
+
+;;---------------------------------------------
+;; コンジェスチョン・シティのためのエッジリスト
+;;---------------------------------------------
+(defun make-city-edges ()
+  (let* ((nodes (loop for i from 1 to *node-num*
+                      collect i))
+         (edge-list (connect-all-islands nodes (make-edge-list)))
+         (cops (remove-if-not (lambda (x)
+                                (zerop (random *cop-odds*)))
+                              edge-list)))
+    (add-cops (edges-to-alist edge-list) cops)))
+
+(defun edges-to-alist (edge-list)
+  (mapcar (lambda (node1)
+            (cons node1
+                  (mapcar (lambda (edge)
+                            (list (cdr edge)))
+                          (remove-duplicates (direct-edges node1 edge-list)
+                                             :test #'equal))))
+          (remove-duplicates (mapcar #'car edge-list))))
+
+(defun add-cops (edge-alist edges-with-cops)
+  (mapcar (lambda (x)
+            (let ((node1 (car x))
+                  (node1-edges (cdr x)))
+              (cons node1
+                    (mapcar (lambda (edge)
+                              (let ((node2 (car edge)))
+                                (if (intersection (edge-pair node1 node2)
+                                                  edges-with-cops
+                                                  :test #'equal)
+                                  (list node2 'cops)
+                                  edge)))
+                            node1-edges))))
+          edge-alist))
+
+;; for debug
+(defun make-city-edges2 ()
+  (let* ((nodes     (get-nodes))
+         (edge-list (connect-all-islands nodes (make-edge-list)))
+         (cops      (remove-if-not #'get-cop edge-list)))
+
+    (add-cops (edges-to-alist edge-list) cops)))
+
+(defun get-cop (x)
+  (let* ((num (random *cop-odds*))
+         (t-f (zerop num)))
+    (print num)
+    (print '-)
+    (prin1 x)
+    t-f))
+
+(defun get-nodes ()
+  ( loop for i from 1 to *node-num*
+         collect i))
+
+(defun edges-to-alist2 (edge-list)
+  (mapcar (lambda (node1)
+            (cons node1
+                  (mapcar (lambda (edge)
+                            (list (cdr edge)))
+                          (remove-duplicates (direct-edges node1 edge-list)
+                                             :test #'equal))))
+          (get-nodes)))
+
+(defun make-city-nodes (edge-alist)
+  (let ((wumpus (random-node))
+        (glow-worms (loop for i below *worm-num*
+                          collect (random-node))))
+    (loop for n from 1 to *node-num*
+          collect (append (list n)
+                          (cond ((eql n wumpus) '(wumpus))
+                                ((within-two n wumpus edge-alist) '(blood!)))
+                          (cond ((member n glow-worms)
+                                 '(glow-worm))
+                                ((some (lambda (worm)
+                                         (within-one n worm edge-alist))
+                                       glow-worms)
+                                 '(lights!)))
+                          (when (some #'cdr (cdr (assoc n edge-alist)))
+                            '(sirens!))))))
+
+;;---------------------------------------------
+;; 初期化
+;;---------------------------------------------
+(defun new-game ()
+  (setf *congestion-city-edges* (make-city-edges))
+  (setf *congestion-city-nodes* (make-city-nodes *congestion-city-edges*))
+  (setf *player-pos*            (find-empty-node))
+  (setf *visited-nodes*         (list *player-pos*))
+  (draw-city))
+
+(defun find-empty-node ()
+  (let ((x (random-node)))
+    (if (cdr (assoc x *congestion-city-nodes*))
+      (find-empty-node)
+      x)))
+
+;;---------------------------------------------
+;; シティのマップを描く
+;;---------------------------------------------
+(defun draw-city ()
+  (ugraph->png "city" *congestion-city-nodes* *congestion-city-edges*))
+
+;;---------------------------------------------
+;; 部分的な知識からシティを描く
+;;---------------------------------------------
+;; 既知のノード
+(defun known-city-nodes ()
+  (mapcar (lambda (node)
+            (if (member node *visited-nodes*)
+              (let ((n (assoc node *congestion-city-nodes*)))
+                (if (eql node *player-pos*)
+                  (appned n '(*))
+                  n))
+              (list node '?)))
+          (remove-duplicates
+            (append *visited-nodes*
+                    (mapcan (lambda (node)
+                              (mapcar #'car
+                                      (cdr (assoc node
+                                                  *congestion-city-edges*))))
+                            (*visited-nodes*))))))
+
+;;---------------------------------------------
+;; コンジェスチョン・シティのノードリスト
+;;---------------------------------------------
+
+(defun neighbors (node edge-alist)
+  (mapcar #'car (cdr (assoc node edge-alist))))
+
+(defun within-one (a b edge-alist)
+  (member b (neighbors a edge-alist)))
+
+(defun within-two (a b edge-alist)
+  (or (within-one a b edge-alist)
+      (some (lambda (x)
+              (within-one x b edge-alist))
+            (neighbors a edge-alist))))
