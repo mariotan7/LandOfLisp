aaf0a69c1fec98dcd74e89ecc5ad0b6a123054d3
Yusuke Tanimoto <mariotan7@gmail.com>
2014-06-23 13:43:32 +0900

    実装完了

--- chap09/OrcButtle.lisp
+++ chap09/OrcButtle.lisp
@@ -232,3 +232,27 @@
     (when (zerop (random 2))
       (princ "It also squirts in your face, taking away a health point! ")
       (decf *player-health* ))))
+
+;;-----------------------------------------
+;; 狡猾なブリガント
+;;-----------------------------------------
+(defstruct (brigand (:include monster)))
+
+(push #'make-brigand *monster-builders*)
+
+(defmethod monster-attack ((m brigand))
+  (let ((x (max *player-health* *player-agility* *player-strength*)))
+    (cond ((= x *player-health*)
+           (princ "A brigand hits you with his slingshot, 
+                  taking off 2 health points! ")
+           (decf *player-health* 2))
+          ((= x *player-agility*)
+           (princ "A brigand catches your leg with his whip, 
+                  taking off 2 agility points! ")
+           (decf *player-agility* 2))
+          ((= x *player-strength*)
+           (princ "A brigand cuts your arm with his whip, 
+                  taking off 2 strength points! ")
+           (decf *player-strength* 2)))))
+           
+
